6a22aa48a4e30559282047263ba46bfdcc4536af
Ripon Kumar Saha <48709421+Riponcs@users.noreply.github.com>
2019-04-22 17:44:06 +0900

    Maintain aspect ration and crop side

--- Reszie-All-Images-In-Folder.m
+++ Reszie-All-Images-In-Folder.m
@@ -1,21 +1,26 @@
 clc
 clear
 
-ImgWidth = 1000;
-ImgHeight = 500;
+ImgWidth = 512;
+ImgHeight = 512;
 
 eachFiles=ls;
 for i=1:length(eachFiles)
     try
-        singleImage=imread(eachFiles(i,:));
-        singleImage=imresize(singleImage,[ImgHeight, ImgWidth]);
+        singleImage=rgb2gray(imread(eachFiles(i,:)));
+        [x,y] = size(singleImage);
+        singleImage=imresize(singleImage,[ImgHeight, int32(y*ImgHeight/x)]); %Resizing with maintaining aspect ratio
+        [x,y] = size(singleImage);
+        singleImage2 = imcrop(singleImage,[round((y-ImgWidth)/2) 0 ImgWidth ImgHeight]);
         fileName=find(eachFiles(i, :)=='.');
         extention=eachFiles(i, fileName+1:end);
         extention(extention==' ')=[];
         name=eachFiles(i,:);
         name(name==' ')=[];
-        imwrite(singleImage, name, extention)
+        imwrite(singleImage2, name, extention)
     catch
         fprintf("Operation is not Successful\n")
     end
 end
+
+%round((int32(ImgWidth*ImgHeight/x)-ImgWidth)/2)
