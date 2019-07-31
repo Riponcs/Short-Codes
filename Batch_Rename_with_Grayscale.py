843ab176244c8f4dc3f8caead7ffbd0803f684b4
Ripon Kumar Saha <48709421+Riponcs@users.noreply.github.com>
2019-08-01 02:06:51 +0900

    Update Batch_Rename_with_Grayscale.py

--- Batch_Rename_with_Grayscale.py
+++ Batch_Rename_with_Grayscale.py
@@ -9,10 +9,8 @@ path = "data/meibomianGland/train/label/exp/"
 for filename in glob.glob(f"{path}*.*"):
     name, extension = os.path.splitext(os.path.basename(filename))
     img2 = cv2.imread(filename)
-    print(filename, "\t Has shape = ", img2.shape)
     im = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
-    print("Last shape = ", im.size, "Object = ", im)
+    print(filename, "\t Previous shape = ", img2.shape, "\tNow = ", im.shape)
     im2 = Image.fromarray(im)
-    print("Last shape = ", im2)
     mpimage.imshow(im2)
     im2.save(f"{path}{name}.png")
